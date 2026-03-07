e9e514928b94c89a1ae552c440383424284b0578
Brice Vinee <sorzy@Ordinateur-portable-de-Brice.local>
2026-03-07 18:19:33 +0100

    build: dist mis a jour pour deploiement

--- dist/assets/v2-CbfZsGhQ.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,r,R as s,a as o}from"./index-DPuecVek.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{"code-path":"src/main-v2.tsx:8:3",children:t.jsx(s,{"code-path":"src/main-v2.tsx:9:5",children:t.jsx(o,{"code-path":"src/main-v2.tsx:10:7"})})}));
