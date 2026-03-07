4b815cad1d10f0498b2ae4ba4a5e7405ad680fac
Brice Vinee <sorzy@Ordinateur-portable-de-Brice.local>
2026-03-07 18:28:38 +0100

    fix: lenis manquant dans package.json + type TS SmoothScroll

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -11,7 +11,7 @@ export function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       smoothWheel: true,
     });
